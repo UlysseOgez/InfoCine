0b62e09f1cf8ec158c3140ac3d22d1eb59b97f62
Ulysse Ogez <ulysseogez@gmail.com>
2019-02-01 14:33:50 +0100

    Ajout de la classe Avis

---
+++ InfoCine/Avis.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfoCine
+{
+    class Avis
+    {
+        private int score;
+        private int filmId;
+        private int personneId;
+        private string text;
+
+        public int Score { get => score; set => score = value; }
+        public int FilmId { get => filmId; set => filmId = value; }
+        public int PersonneId { get => personneId; set => personneId = value; }
+        public string Text { get => text; set => text = value; }
+
+        public Avis()
+        {
+        }
+
+        public Avis(int score, int filmId, int personneId, string text)
+        {
+            Score = score;
+            FilmId = filmId;
+            PersonneId = personneId;
+            Text = text;
+        }
+    }
+}
