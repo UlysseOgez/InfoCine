420c7954b3eebd7004a0316df886e1f6720dc882
Ulysse Ogez <ulysseogez@gmail.com>
2019-02-01 12:10:34 +0100

    Ajout de la classe Film

---
+++ InfoCine/Film.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfoCine
+{
+    class Film
+    {
+        private string titre;
+        private DateTime dateSortie;
+
+        public string Titre { get => titre; set => titre = value; }
+        public DateTime DateSortie { get => dateSortie; set => dateSortie = value; }
+
+        public Film()
+        {
+        }
+
+        public Film(string titre, DateTime dateSortie)
+        {
+            Titre = titre;
+            DateSortie = dateSortie;
+        }
+    }
+}
